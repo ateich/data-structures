1c9d1ad300008052a73386b904eeb0b740fb5275
Andrew Teich <hackreactor@a673.local>
2014-09-10 12:25:07 -0700

    Added the prototypal pattern to Sprint One

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.items = {};
+  queue.firstInLine = 1;
+  return queue;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+	this.items[Object.keys(this.items).length+this.firstInLine] = value;
+};
+
+queueMethods.dequeue = function(){
+	if(Object.keys(this.items).length > 0){
+		var temp = this.items[this.firstInLine];
+		delete this.items[this.firstInLine];
+		this.firstInLine++;
+		return temp;
+	} else {
+		return null;
+	}
+};
+
+queueMethods.size = function(){
+	return Object.keys(this.items).length;
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.items = {};
+  return stack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(val){
+	this.items[this.size()] = val;
+};
+
+stackMethods.pop = function(){
+	var key = this.size()-1;
+	var temp = this.items[key];
+	delete this.items[key];
+	return temp;
+};
+
+stackMethods.size = function(){
+	return Object.keys(this.items).length;
+};
 
