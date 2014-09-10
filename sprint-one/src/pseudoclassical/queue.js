ac551b3e64b94f9940caab0e614729fc77f2d8e3
Andrew Teich <hackreactor@a673.local>
2014-09-10 12:35:32 -0700

    Added pseudoclassical pattern to Sprint One

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.items = {};
+  this.firstInLine = 1;
+};
+
+Queue.prototype.enqueue = function(val){
+	this.items[this.size()+this.firstInLine] = val;
+};
+
+Queue.prototype.dequeue = function(){
+	var temp = this.items[this.firstInLine];
+	delete this.items[this.firstInLine];
+	this.firstInLine++;
+	return temp;
+};
+
+Queue.prototype.size = function(){
+	return Object.keys(this.items).length;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.items = {};
+};
+
+Stack.prototype.push = function(val){
+	this.items[this.size()] = val;
+};
+
+Stack.prototype.pop = function(){
+	var key = this.size()-1;
+	var temp = this.items[key];
+	delete this.items[key];
+	return temp;
+};
+
+Stack.prototype.size = function(){
+	return Object.keys(this.items).length;
 };
 
 
