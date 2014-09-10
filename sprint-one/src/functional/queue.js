ad53cb5ebed03cdf8e6bf958e463d68eabb0500f
Andrew Teich <hackreactor@a673.local>
2014-09-10 11:51:10 -0700

    Completed Functional Pattern for Sprint One

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var makeQueue = function(){
   var someInstance = {};
+  var firstInLine = 1;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +8,19 @@ var makeQueue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    var newKey = firstInLine + Object.keys(storage).length;
+    storage[newKey] = value;
   };
 
   someInstance.dequeue = function(){
+    var toReturn = storage[firstInLine];
+    delete storage[firstInLine];
+    firstInLine++;
+    return toReturn;
   };
 
   someInstance.size = function(){
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,25 @@
 var makeStack = function(){
   var someInstance = {};
+  //var highestKey = 1;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[Object.keys(storage).length] = value;
   };
 
   someInstance.pop = function(){
+    //highestKey--;
+    var key = Object.keys(storage).length-1;
+    var toReturn = storage[key];
+    delete storage[key];
+    return toReturn;
   };
 
   someInstance.size = function(){
+    return Object.keys(storage).length;
   };
 
   return someInstance;
