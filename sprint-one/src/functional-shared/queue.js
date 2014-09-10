d000adc9e497340f00b5ec21ef243fbca13bc480
Andrew Teich <hackreactor@a673.local>
2014-09-10 12:12:49 -0700

    Added functional-shared pattern for Sprint One

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,31 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.firstInLine = 1;
+  queue.itemsInLine = {};
+  _.extend(queue, queueMethods);
+
+  return queue;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(value){
+	this.itemsInLine[Object.keys(this.itemsInLine).length + this.firstInLine] = value;
+};
+
+queueMethods.dequeue = function(){
+	var toReturn = this.itemsInLine[this.firstInLine];
+	delete this.itemsInLine[this.firstInLine];
+	this.firstInLine++;
+	return toReturn;
+};
+
+queueMethods.size = function(){
+	return Object.keys(this.itemsInLine).length;
+};
+
+var newQueue = makeQueue();
 
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.items = {};
+  _.extend(stack, stackMethods);
+  return stack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+	this.items[Object.keys(this.items).length] = value;
+};
+
+stackMethods.pop = function(){
+	var key = Object.keys(this.items).length-1;
+	var temp = this.items[key];
+	delete this.items[key];
+	return temp;
+};
+
+stackMethods.size = function(){
+	return Object.keys(this.items).length;
+};
+
+var newStack = makeStack();
 
 
