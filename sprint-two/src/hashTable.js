68cdd4ebd14b7234bf229c2da1c2631871944fbc
ateich <andrewteich@me.com>
2014-09-12 10:39:04 -0700

    Fixing HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,34 +5,49 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var o = {};
-  o.val = v;
-  o.key = k;
-  if(Array.isArray(this._storage[i])){
-    this._storage[i].push(o);
-  } else {
-    this._storage[i] = [o];
-  }
+
+  //collision detection
+  //  call retrieve
+  //  if returns value
+  //    we will have a collision
+  //    if returns obj
+  //      push to object new obj {key: v}
+  //    else
+  //      make obj {old_key:old_val, key:v}
+
+  //Check for doubling
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  for(var j = 0; j < this._storage[i].length; j++){
-    if(this._storage[i][j] && this._storage[i][j].key === k){
-      return this._storage[i][j].val;
-    }
-  }
+  // if storage get i is an object with multiple sub objects
+  //  iterate through object keys to find k
+  //  return obj[k];
+  // else return it
   return null;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  for (var j = 0; j < this._storage[i].length; j++){
-    if(this._storage[i][j].key === k){
-      this._storage[i][j] = null;
-    }
-  }
+  //each with callback
+  //callback = function that checks if item's index is i
+  //callback params (value, index, array)
+  //if found item to remove, remove it from callback array
 };
+
+
+//Storage Mutator
+//params (isDoubling)
+//if isDoubling
+//  limit *=2
+//else
+//  limit /=2
+//makeLimitedArray using limit
+//use each to get value of each current index
+//get key from hash:key container
+//calculate new hash from key using new limit
+//add value from old storage to new storage
+//set _.storage to new storage
 
 
 /*
