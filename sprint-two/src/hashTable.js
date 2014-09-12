ceedf1ef9f08db2f9c0d8ffde7c7a7a257c78c86
ateich <andrewteich@me.com>
2014-09-12 11:40:22 -0700

    Refactored Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,27 +8,18 @@ HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var obj = [k, v];
   var getStorage = this._storage.get(i);
+
   if(!Array.isArray(getStorage)){
     this._storage.set(i, [obj]);
-
   } else {
     getStorage.push(obj);
     console.log("There is a collision");
     this._storage.set(i, getStorage);
   }
+
   this._usedIndexes++;
-  //collision detection
-  //  call retrieve
-  //  if !returns array
-  //    add emtpy array
-  //
-  //  push {k:v} to array
 
-  //Check for doubling
-
-  console.log("Used Index Count: " + this._usedIndexes);
   if(this._usedIndexes === this._limit * 3 / 4){
-    debugger;
     this.changeHashSize(true);
   }
 };
@@ -42,14 +33,8 @@ HashTable.prototype.retrieve = function(k){
         return getStorage[j][1];
       }
     }
-    return null;
-  } else {
-    return null;
   }
-  // if storage get i is an object with multiple sub objects
-  //  iterate through object keys to find k
-  //  return obj[k];
-  // else return it
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
@@ -67,24 +52,15 @@ HashTable.prototype.remove = function(k){
       }
     }
   });
-  //each with callback
-  //callback = function that checks if item's index is i
-  //callback params (value, index, array)
-  //if found item to remove, remove it from callback array
+
   if(this._usedIndexes * 2 < this._limit){
     this.changeHashSize(false);
   }
 };
 
 HashTable.prototype.changeHashSize = function(isDoubling){
-  console.log("is doubling/halfing " + isDoubling);
-  console.log("limit before change: " + this._limit);
-  if(isDoubling){
-    this._limit *= 2;
-  } else {
-    this._limit /= 2;
-  }
-  console.log("limit after change: " + this._limit);
+  isDoubling ? this._limit *= 2 : this._limit /= 2;
+
   var newHash = makeLimitedArray(this._limit);
   var useThis = this;
   var tempStorage = this._storage;
@@ -99,22 +75,3 @@ HashTable.prototype.changeHashSize = function(isDoubling){
     }
   });
 };
-
-//Storage Mutator
-//params (isDoubling)
-//if isDoubling
-//  limit *=2
-//else
-//  limit /=2
-//makeLimitedArray using limit
-//use each to get value of each current index
-//get key from hash:key container
-//calculate new hash from key using new limit
-//add value from old storage to new storage
-//set _.storage to new storage
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
