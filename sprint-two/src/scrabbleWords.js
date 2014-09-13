2a7695721062a0ce2ce9073d906939bbc1824ffb
ateich <andrewteich@me.com>
2014-09-13 14:25:24 -0700

    Started Scrabble Extra Credit

---
+++ sprint-two/src/scrabbleWords.js
@@ -0,0 +1,47 @@
+//Scrabble
+
+//Find English words given letter
+//(Scrabble Letters)
+
+//Scrabble Letter Frequencies
+var Scrabble = function(){
+  this.lettersInHand = [];
+  this.arrayOfTiles = [];
+  this.leterFrequency = {
+    'A':9,
+    'B':2,
+    'C':2,
+    'D':4,
+    'E':12,
+    'F':2,
+    'G':3,
+    'H':2,
+    'I':9,
+    'J':1,
+    'K':1,
+    'L':4,
+    'M':2,
+    'N':6,
+    'O':8,
+    'P':2,
+    'Q':1,
+    'R':6,
+    'S':4,
+    'T':6,
+    'U':4,
+    'V':2,
+    'W':2,
+    'X':1,
+    'Y':2,
+    'Z':1,
+    'Blank':2
+  };
+}; //Percentage
+
+Scrabble.prototype.getTiles = function(numTiles){
+  for(var i=0; i<numTiles; i++)
+  {
+
+  }
+}
+
